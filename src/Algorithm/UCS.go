346baf09d540b1120d952b42a2b62f489098ce0d
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-04-12 12:15:53 +0700

    feat: clean program

--- src/Algorithm/UCS.go
+++ src/Algorithm/UCS.go
@@ -1,70 +1,70 @@
 package Algorithm
 
 import (
- "container/heap"
- "fmt"
+	"container/heap"
+	"fmt"
 )
 
 func UCS(adjMatrix [][]float64, nodeIndex map[string]int, goal string, start string) ([]string, float64) {
- pq := make(PriorityQueue, 1)
+    fmt.Println("You are using UCS Algorithm")
+	pq := make(PriorityQueue, 1)
 
- tempAdjMatrix := CopyMatrix(adjMatrix)
+	tempAdjMatrix := CopyMatrix(adjMatrix)
 
- distance := 0.0
+	distance := 0.0
 
- path := []string{start}
+	path := []string{start}
 
- visited := make(map[string]bool)
- visited[start] = true
+	visited := make(map[string]bool)
+	visited[start] = true
 
- itemValue := []interface{}{
-  path,
-  start,
-  distance,
- }
- pq[0] = &Item{
-  value:    itemValue,
-  priority: distance,
-  index:    0,
- }
- heap.Init(&pq)
+	itemValue := []interface{}{
+		path,
+		start,
+		distance,
+	}
+	pq[0] = &Item{
+		value:    itemValue,
+		priority: distance,
+		index:    0,
+	}
+	heap.Init(&pq)
 
- for pq.Len() > 0 {
-  currentNode := heap.Pop(&pq).(*Item)
-  currentValue := currentNode.value.([]interface{})
-  nodeName := currentValue[1].(string)
-  visited[nodeName] = true
-  currentIdx := nodeIndex[nodeName]
-  currentPath := currentValue[0].([]string)
-  distanceCost := currentValue[2].(float64)
-  fmt.Println("This is the path now", currentPath)
-  if nodeName == goal {
-   path = currentPath
-   distance = distanceCost
-   break
-  }
+	for pq.Len() > 0 {
+		currentNode := heap.Pop(&pq).(*Item)
+		currentValue := currentNode.value.([]interface{})
+		nodeName := currentValue[1].(string)
+		visited[nodeName] = true
+		currentIdx := nodeIndex[nodeName]
+		currentPath := currentValue[0].([]string)
+		distanceCost := currentValue[2].(float64)
+		if nodeName == goal {
+			path = currentPath
+			distance = distanceCost
+			break
+		}
 
-  for idx, val := range tempAdjMatrix[currentIdx] {
-   if val != 0 {
-    nextNode, found := GetKeyByValue(nodeIndex, idx)
-    if found && !visited[nextNode] {
-     tempCurrentPath := append([]string{}, currentPath...) // create a copy of currentPath
-     tempCurrentPath = append(tempCurrentPath, nextNode)
-     tempDistanceCost := distanceCost + val
-     tempItemValue := []interface{}{
-      tempCurrentPath,
-      nextNode,
-      tempDistanceCost,
-     }
-     tempItem := &Item{
-      value:    tempItemValue,
-      priority: tempDistanceCost,
-     }
-     heap.Push(&pq, tempItem)
-    }
-   }
-  }
- }
+		for idx, val := range tempAdjMatrix[currentIdx] {
+			if val != 0 {
+				nextNode, found := GetKeyByValue(nodeIndex, idx)
+				if found && !visited[nextNode] {
+					tempCurrentPath := append([]string{}, currentPath...) // create a copy of currentPath
+					tempCurrentPath = append(tempCurrentPath, nextNode)
+					tempDistanceCost := distanceCost + val
+					tempItemValue := []interface{}{
+						tempCurrentPath,
+						nextNode,
+						tempDistanceCost,
+					}
+					tempItem := &Item{
+						value:    tempItemValue,
+						priority: tempDistanceCost,
+					}
+					heap.Push(&pq, tempItem)
+				}
+			}
+		}
+	}
 
- return path, distance
+	return path, distance
 }
